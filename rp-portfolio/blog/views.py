57e00ca936f5f271c1635c6840189acdee006462
root <root@coder.cyberbeach.com>
2019-06-05 13:20:34 +0100

    the project almost done

--- rp-portfolio/blog/views.py
+++ rp-portfolio/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
@@ -14,5 +15,15 @@ def blog_category(request, category):
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
-    context = {"post": post, "comments": comments,}
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+            author=form.cleaned_data["author"],
+            body=form.cleaned_data["body"],
+            post=post,
+            )
+            comment.save()
+    context = {"post": post, "comments": comments, "form": form}
     return render(request, "blog_detail.html", context)
