41a97d4ccdc54c5ab8ff00b44b54159dcd928e81
root <root@coder.cyberbeach.com>
2019-06-05 12:06:37 +0100

    last update

--- rp-portfolio/projects/models.py
+++ rp-portfolio/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    technologie = models.CharField(max_length=20, blank=True)
+    image = models.FilePathField(path="projects/static/projects/img")
